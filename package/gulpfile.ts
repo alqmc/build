6265ad03fc478943ca1c3a82506ba55661c48d17
Yonghu <Yonghu520@outlook.com>
2022-04-24 22:15:58 +0800

    feat: init

---
+++ package/gulpfile.ts
@@ -0,0 +1,16 @@
+import { series, parallel } from 'gulp';
+import { run, withTask } from './process';
+import { createZip } from './buildZip';
+import { copyFiles } from './copyfile';
+import { buildLib } from './build';
+import { buildStyles } from './buildStyle';
+export default series(
+  // withTask('update:version', () => run('pnpm run update:version')),
+  withTask('clear', () => run('pnpm run clear')),
+  parallel(
+    buildLib,
+    withTask('build:types', () => run('pnpm build:types'))
+  ),
+  parallel(buildStyles, copyFiles),
+  createZip
+);
